acb9e6076a715b9e367a9aa14f37a33b09db8927
NickMcMichael <31061919+NickMcMichael@users.noreply.github.com>
2017-10-09 12:09:21 +0200

    Add files via upload

---
+++ Tank Game/Tank Game/Tank/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Tank
+{
+    private int barrelPos { public get; set; }
+    private int tankPosx {public get; set;} 
+    private int tankPosy { public get; set; }
+
+    public void moveRight()
+    {
+        tankPosx += 1;
+    }
+    public void moveLeft()
+    {
+        tankPosx -= 1;
+    }
+    public void barrelUp()
+    {
+        barrelPos += 1;
+    }
+    public void barrelDown()
+    {
+        barrelPos -= 1;
+    }
+	public Tank(int x, int y)
+	{
+        tankPosx = x;
+        tankPosy = y;
+        barrelPos = 90;
+	}
+}
---
+++ Tank Game/Tank Game/World/World.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class World
+{
+    public int player1Score { get; private set; }
+    public int player2Score { get; private set; }
+    private double gravity { public get; set; }
+    private double wind { public get; set; }
+
+	public World()
+	{
+	}
+}
